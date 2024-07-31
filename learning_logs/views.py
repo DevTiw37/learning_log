d48c4d33136265b23e48bb9ca65065e7b02da03e
DevTiw37 <tiwaridevesh937@gmail.com>
2024-07-31 18:10:14 +0530

    new

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Topic
 
 def index(request):
     """The home page for Learning Logs."""
     return render(request, 'learning_logs/index.html')
+
+def topics(requst):
+    """Show all topics."""
+    topics = Topic.objects.order_by('date_added')
+    context = { 'topics': topics }
+    return render(request, 'learning_logs/topics.html', context)
